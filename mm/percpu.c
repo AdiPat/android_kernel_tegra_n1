dea7f7622ef1dd3c9113f04517944c87503ed8b9
Sarthak Acharya <sa6915@gmail.com>
2012-03-26 22:16:33 +0530

    percpu: zero memory more efficiently in mm/percpu.c::pcpu_mem_alloc()

--- mm/percpu.c
+++ mm/percpu.c
@@ -286,13 +286,8 @@ static void *pcpu_mem_alloc(size_t size)
 		return NULL;
 
 	if (size <= PAGE_SIZE)
-		return kzalloc(size, GFP_KERNEL);
-	else {
-		void *ptr = vmalloc(size);
-		if (ptr)
-			memset(ptr, 0, size);
-		return ptr;
-	}
+	else 
+        return vzalloc(size);
 }
 
 /**

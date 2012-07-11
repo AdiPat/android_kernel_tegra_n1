9342934f2329d3f44c733a472928292361f97885
AdiPat <adithemagnificent@gmail.com>
2012-07-12 00:13:57 +0530

    tegra-n1: Remove reservation of memory for RAM CONSOLE
    Also Disable RAM Console in defconfig

--- arch/arm/mach-tegra/board-n1.c
+++ arch/arm/mach-tegra/board-n1.c
@@ -2921,10 +2921,10 @@ void __init tegra_n1_reserve(void)
 	else	/* 1GB */
 		tegra_reserve(SZ_256M, SZ_8M, SZ_16M);
 
-	/* Reserve memory for the ram console. */
-	ram_console_start = memblock_end_of_DRAM() - SZ_1M;
+	/* Reserve memory for the ram console.
+	ram_console_start = memblock_end_of_DRAM() - SZ_1M;  //Disabled RAM Console 
 
-	ret = memblock_remove(ram_console_start, SZ_1M);
+	ret = memblock_remove(ram_console_start, SZ_1M); 
 	if (ret < 0) {
 		pr_err("Failed to reserve 0x%x bytes for ram_console at "
 			"0x%llx, err = %d.\n",
@@ -2932,7 +2932,7 @@ void __init tegra_n1_reserve(void)
 	} else {
 		ram_console_resource[0].start = ram_console_start;
 		ram_console_resource[0].end = ram_console_start + SZ_1M - 1;
-	}
+	} */
 }
 
 MACHINE_START(TEGRA_GENERIC, "n1")

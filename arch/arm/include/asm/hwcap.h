688ad361154f67502952fac31c81d0042dde1137
AdiPat <adithemagnificent@gmail.com>
2012-07-11 23:27:25 +0530

    Fix git errors from previous commit-

--- arch/arm/include/asm/hwcap.h
+++ arch/arm/include/asm/hwcap.h
@@ -4,24 +4,6 @@
 /*
  * HWCAP flags - for elf_hwcap (in kernel) and AT_HWCAP
  */
-<<<<<<< HEAD
-#define HWCAP_SWP	1
-#define HWCAP_HALF	2
-#define HWCAP_THUMB	4
-#define HWCAP_26BIT	8	/* Play it safe */
-#define HWCAP_FAST_MULT	16
-#define HWCAP_FPA	32
-#define HWCAP_VFP	64
-#define HWCAP_EDSP	128
-#define HWCAP_JAVA	256
-#define HWCAP_IWMMXT	512
-#define HWCAP_CRUNCH	1024
-#define HWCAP_THUMBEE	2048
-#define HWCAP_NEON	4096
-#define HWCAP_VFPv3	8192
-#define HWCAP_VFPv3D16	16384
-#define HWCAP_TLS	32768
-=======
 #define HWCAP_SWP	(1 << 0)
 #define HWCAP_HALF	(1 << 1)
 #define HWCAP_THUMB	(1 << 2)
@@ -38,7 +20,6 @@
 #define HWCAP_VFPv3	(1 << 13)
 #define HWCAP_VFPv3D16	(1 << 14)
 #define HWCAP_TLS	(1 << 15)
->>>>>>> e4324ca... ARM: hwcaps: use shifts instead of hardcoded constants
 
 #if defined(__KERNEL__) && !defined(__ASSEMBLY__)
 /*

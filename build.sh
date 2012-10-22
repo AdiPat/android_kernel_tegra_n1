19dd71c568b5ba07ea683e58d54f8692d4945f25
AdiPat <adithemagnificent@gmail.com>
2012-10-22 18:54:52 +0530

    Tweak my build script

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+ROOT="/home/aditya/i9103"
+CROSS_COMPILE="/home/aditya/Toolchain/arm-eabi-linaro-4.6.2/bin/arm-eabi"
+KERNEL_DIR="/home/aditya/i9103/Kernel"
+RAMDISK_DIR="/home/aditya/i9103/ramdisk"
+MODULES_DIR="$RAMDISK_DIR/lib/modules"
+OUT="/home/aditya/i9103/out"
+
 echo "|~~~~~~~~COMPILING TITANIUM KERNEL ~~~~~~~~~~~|"
 echo "|---------------------------------------------|"
 echo "Cleaning source"
@@ -7,7 +14,8 @@ rm *.img
 rm *.gz
 cd out 
 rm *.img
-cd ../Kernel  
+cd $KERNEL_DIR 
+export CROSS_COMPILE=$CROSS_COMPILE-
 make clean mrproper
 echo "Importing defconfig"
 make tegra_n1_defconfig
@@ -16,22 +24,22 @@ read $version
 echo ">> COMPILING! >>>"
 make -j84
 echo "Copying modules and stripping em"
-find -name '*.ko' -exec cp -av {} ../ramdisk/lib/modules/ \;
-cd ../ramdisk/lib/modules
+find -name '*.ko' -exec cp -av {} $MODULES_DIR/ \;
+cd $MODULES_DIR
 echo "Strip modules for size"
 
 for m in $(find . | grep .ko | grep './')
 do
         echo $m
 
-/home/aditya/Toolchain/arm-eabi-4.4.3/bin/arm-eabi-strip --strip-unneeded $m
+$CROSS_COMPILE-strip --strip-unneeded $m
 done
-cd ~/i9103/Kernel 
+cd $KERNEL_DIR
 echo "Packing Ramdisk"
-cd ..
-./mkbootfs ramdisk | gzip > ramdisk.gz
-./mkbootimg --kernel Kernel/arch/arm/boot/zImage --ramdisk ramdisk.gz -o out/boot.img --base 10000000
-cd out
+cd $ROOT
+./mkbootfs $RAMDISK_DIR | gzip > ramdisk.gz
+./mkbootimg --kernel $KERNEL_DIR/arch/arm/boot/zImage --ramdisk ramdisk.gz -o $OUT/boot.img --base 10000000
+cd $OUT
 echo "CLear old zip files"
 rm *.zip
 echo "Making CWM Flashable zip"

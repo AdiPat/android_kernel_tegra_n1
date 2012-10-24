1649cfa7f0ecd6dade311f1846e5c87b43153fe2
AdiPat <adithemagnificent@gmail.com>
2012-10-24 13:56:15 +0530

    Add CM10 support.
    Signed-off-by: AdiPat <adithemagnificent@gmail.com>

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ ROOT="/home/aditya/i9103"
 CROSS_COMPILE="/home/aditya/Toolchain/arm-eabi-linaro-4.6.2/bin/arm-eabi"
 KERNEL_DIR="/home/aditya/i9103/Kernel"
 RAMDISK_DIR="/home/aditya/i9103/ramdisk"
+RAMDISK_CM_DIR="/home/aditya/i9103/ramdisk-cm10"
 MODULES_DIR="$RAMDISK_DIR/lib/modules"
 OUT="/home/aditya/i9103/out"
 
@@ -20,7 +21,7 @@ make clean mrproper
 echo "Importing defconfig"
 make tegra_n1_defconfig
 echo "Please Enter Release Version" 
-read $v
+read $vrsn
 echo ">> COMPILING! >>>"
 make -j84
 echo "Copying modules and stripping em"
@@ -38,18 +39,34 @@ cd $KERNEL_DIR
 echo "Packing Ramdisk"
 cd $ROOT
 ./mkbootfs $RAMDISK_DIR | gzip > ramdisk.gz
+./mkbootfs $RAMDISK_CM_DIR | gzip > ramdisk-cm10.gz 
 ./mkbootimg --kernel $KERNEL_DIR/arch/arm/boot/zImage --ramdisk ramdisk.gz -o $OUT/boot.img --base 10000000
+./mkbootimg --kernel $KERNEL_DIR/arch/arm/boot/zImage --ramdisk ramdisk-cm10.gz $OUT/boot_cm10.img --base 10000000
 cd $OUT
 echo "CLear old zip files"
 rm *.zip
 echo "Making CWM Flashable zip"
-zip -r GT-I9103_TITANIUM_KERNEL_BUILD_$v.zip META-INF boot.img
+zip -r GT-I9103_ICS_TITANIUM_KERNEL_BUILD#$vrsn.zip META-INF boot.img
 
 echo "Signing the zip file"
 
-java -jar signapk.jar testkey.x509.pem testkey.pk8 GT-I9103_TITANIUM_KERNEL_BUILD_$v.zip SIGNED_GT-I9103_TITANIUM_KERNEL_BUILD_$v.zip.zip
+java -jar signapk.jar testkey.x509.pem testkey.pk8 GT-I9103_ICS_TITANIUM_KERNEL_BUILD#$vrsn.zip SIGNED_GT-I9103_ICS_TITANIUM_KERNEL_BUILD#$vrsn.zip.zip
 
-rm GT-I9103_TITANIUM_KERNEL_BUILD_$v.zip
+rm GT-I9103_ICS_TITANIUM_KERNEL_BUILD#$vrsn.zip
+rm boot.img 
+mv boot_cm10.img boot.img 
+
+# pack cm10 
+
+echo "Making CWM Flashable zip"
+zip -r GT-I9103_CM10_TITANIUM_KERNEL_BUILD#$vrsn.zip META-INF boot.img
+
+echo "Signing the zip file"
+
+java -jar signapk.jar testkey.x509.pem testkey.pk8 GT-I9103_CM10_TITANIUM_KERNEL_BUILD#$vrsn.zip SIGNED_GT-I9103_CM10_TITANIUM_KERNEL_BUILD#$vrsn.zip.zip
+
+#########
+rm boot.img 
 
 echo "DONE, PRESS ENTER TO FINISH"
 read ANS
